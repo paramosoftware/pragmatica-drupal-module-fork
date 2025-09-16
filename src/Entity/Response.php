665bd3f498f9aac0a9576a5b86098881093982d5
Matheus <dmatheusps@gmail.com>
2025-09-16 20:57:56 -0300

    GPP-51 Criar entity: Response

---
+++ src/Entity/Response.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Drupal\pragmatica\Entity;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * Defines the Response content entity.
+ *
+ * @ContentEntityType(
+ *   id = "pragmatica_response",
+ *   label = @Translation("Resposta"),
+ *   label_plural = @Translation("Respostas"),
+ *   base_table = "pragmatica_response",
+ *   admin_permission = "pragmatica",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "name"
+ *   },
+ *   handlers = {
+ *     "list_builder" = "Drupal\pragmatica\ListBuilder\PragmaticaBaseListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\pragmatica\Form\PragmaticaBaseForm",
+ *       "edit" = "Drupal\pragmatica\Form\PragmaticaBaseForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm"
+ *     }
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/pragmatica/response/{pragmatica_response}",
+ *     "add-form" = "/admin/pragmatica/response/add",
+ *     "edit-form" = "/admin/pragmatica/response/{pragmatica_response}/edit",
+ *     "delete-form" = "/admin/pragmatica/response/{pragmatica_response}/delete",
+ *     "collection" = "/admin/pragmatica/response"
+ *   }
+ * )
+ */
+class Response extends PragmaticaBaseEntity {
+
+  public static function getFieldsIds(): array {
+    return [
+      'id',
+      'name',
+      'situation_id',
+      'informant_id',
+      'source_id',
+      'created',
+      'changed',
+    ];
+  }
+
+  public static function getFieldsToXmlMapping(): array {
+    return parent::addFieldsToXmlMapping([], self::getFieldsIds());
+  }
+
+  public function getListHeaders(): array {
+    $parent = parent::getListHeaders();
+    $header['situation_id'] = t('Situação');
+    $header['informant_id'] = t('Informante');
+    return $this->addItemsAfterKeyInArray($header, $parent, 'name');
+  }
+
+  public function buildListRow(PragmaticaBaseEntity $entity): array {
+    /** @var self $entity */
+    $row = parent::buildListRow($entity);
+    $row['situation_id'] = $entity->get('situation_id')->entity ? $entity->get('situation_id')->entity->label() : '';
+    $row['informant_id'] = $entity->get('informant_id')->entity ? $entity->get('informant_id')->entity->label() : '';
+    return $row;
+  }
+
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type): array {
+    $fields = [];
+
+    $fields['name'] = BaseFieldDefinition::create('string_long')
+      ->setLabel(t('Resposta'))
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'text_textarea',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'type' => 'text_default',
+        'label' => 'above',
+        'weight' => 0,
+      ]);
+
+    $fields['situation_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Situação'))
+      ->setDescription(t('Situação associada'))
+      ->setSetting('target_type', 'pragmatica_situation')
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'entity_reference_label',
+        'weight' => 5,
+      ]);
+
+    $fields['informant_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Informante'))
+      ->setDescription(t('Informante associado'))
+      ->setSetting('target_type', 'pragmatica_informant')
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 6,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'entity_reference_label',
+        'weight' => 6,
+      ]);
+
+    $fields['source_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Fonte'))
+      ->setDescription(t('Fonte associada'))
+      ->setSetting('target_type', 'pragmatica_source')
+      ->setRequired(FALSE)
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 6,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'entity_reference_label',
+        'weight' => 6,
+      ]);
+
+    return self::addBaseFieldDefinitions($fields, self::getFieldsIds());
+  }
+}
